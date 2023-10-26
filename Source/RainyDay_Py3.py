d124cbd4af2f083f6a9f69f76344577a7b8b911e
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-10-26 10:22:53 -0400

    troubleshooting

--- Source/RainyDay_Py3.py
+++ Source/RainyDay_Py3.py
@@ -1369,6 +1369,17 @@ if CreateCatalog:
 print("trimming storm catalog...")
 
 if CreateCatalog==False:
+    # DCL WORK
+    print("duration")
+    print(duration)
+    print("#################################")
+    print("catrain.shape[0]")
+    print(catrain.shape[0])
+    print("#################################")
+    print("rainprop.timeres")
+    print(rainprop.timeres)
+    print("#################################")
+    # END DCL WORK
     if duration>catrain.shape[0]*rainprop.timeres/60.:
         sys.exit("The specified duration is longer than the length of the storm catalog")
 
